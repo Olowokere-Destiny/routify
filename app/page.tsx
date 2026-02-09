18e9b16dc687332676241b50be103c26ed5e28f0
Olowokere_Destiny <destinyolowokere@gmail.com>
2026-02-09 07:13:02 +0100

    add leaflet map

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import MapWrapper from "../components/MapWrapper";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js</h1>
+    <main className="fixed inset-0 h-full w-full">
+      <MapWrapper />
     </main>
   );
 }
